2160e75d7b273532227d0d5b853f5c0c8d0d4549
AKASH <adroit@LAPTOP-0GGHS6JD.localdomain>
2020-07-15 11:27:31 +0530

    Updates

---
+++ precache-manifest.06f97256e56813b45a70ebd585083dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8e33a6218d6604fc6685599af7147c",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "051dabfd87f55f2e6942",
+    "url": "/React/static/css/main.b2374aa7.chunk.css"
+  },
+  {
+    "revision": "096a21affa0f2f9f040a",
+    "url": "/React/static/js/2.549037f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.549037f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051dabfd87f55f2e6942",
+    "url": "/React/static/js/main.bc28907f.chunk.js"
+  },
+  {
+    "revision": "bf7f5f7ddf2dea0d7a0a",
+    "url": "/React/static/js/runtime-main.e4c72ed3.js"
+  }
+]);
